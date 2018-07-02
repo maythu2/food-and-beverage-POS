078e4c487e06d0199be3fad6d9f44e3c27e98bc3
May Thu <maythukyaw@op-j.jp>
2018-07-02 13:13:25 +0630

    for heroku

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,9 +1,9 @@
 <html>
    <head>
         <title>@yield('Food and Beverage Point of Sales Systems')</title>
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet">
-        
+        <link href="{{ asset('css/bootstrap.min.css',env('REDIRECT_HTTPS')) }}" rel="stylesheet">
+        <link href="{{ asset('css/select2.min.css',env('REDIRECT_HTTPS')) }}" rel="stylesheet">
+
         <script src="{{ asset('js/jquery-3.3.1.min.js')}}"></script>        
         <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
         <script src="{{ asset('js/bootstrap.min.js') }}"></script>
